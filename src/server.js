0f3c72bf71b2052d62a73301897e8414d130c66c
sridhar-hp <sridhark3773@gmail.com>
2026-07-18 21:18:24 +0530

    done

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// src/server.js
+require('dotenv').config();
+const http = require('http');
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
