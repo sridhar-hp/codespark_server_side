8c15fb1111c35ee848290d8b32043c2e3bb1a4a0
sridhar-hp <sridhark3773@gmail.com>
2026-07-30 05:15:00 +0530

    initila commit

--- src/routes/githubRoutes.js
+++ src/routes/githubRoutes.js
@@ -1,12 +1,13 @@
 // src/routes/githubRoutes.js
 const express = require('express');
 const router = express.Router();
-const githubController = require('../controllers/githubController');
+const githubController = require('../controllers/githubActivityController');
 const { protect } = require('../middleware/authMiddleware');
+const githubValidate = require('../validations/activityValidation').github;
+const validate = require('../middleware/validationMiddleware');
 
 router.use(protect);
 
-router.post('/connect', githubController.connect);
-router.get('/profile', githubController.getProfile);
-
+router.post('/', githubValidate, validate, githubController.create);
+router.get('/', githubController.list);
 module.exports = router;
